a19b2ee98b4ff0f90b0bad555c82d0e889d2ffac
cstanford <connorgstanford@gmail.com>
2017-07-08 17:07:01 -0500

    Deleting junk.

--- python/legacy/TrendGet.py
+++
@@ -1,8 +0,0 @@
-auth = tweepy.OAuthHandler('WAmc6nksqtBkRXkYHDzGTkNC2', 'rHbTMCkMjZU3Ru4pPrh1GFbPrcGfVyFfUXNOqqueN9cOuMdylv')
-auth.set_access_token('371692355-JUMH5vdqTgUlCGVukLDRFOePL9QrYHni7H79X49I', 'VbsIxYfEkLlBTcfEvJkyO2fNDCCHt0ZKVzKL81FY2RV9V')
-api = tweepy.API(auth)
-
-trends = api.trends_place(1) #worldwide search
-
-with open("Trends.json",'w') as outfile:
-	json.dump(trends, outfile, indent=2)
